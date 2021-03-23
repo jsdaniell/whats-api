6903886f497e54a9a4b4704153138bab2cddcdd5
Daniel <jose.daniell@outlook.com>
2021-03-23 11:18:41 -0300

    adjusted ci script

--- config/config.go
+++ config/config.go
@@ -1,28 +1,9 @@
 package config
 
-import (
-	"github.com/joho/godotenv"
-	"log"
-	"os"
-	"strconv"
-)
-
 var (
 	PORT = 9000
 )
 
 func Load() {
-
-	var err error
-
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	PORT, err = strconv.Atoi(os.Getenv("PORT"))
-	if err != nil {
-		log.Println(err)
-		PORT = 9000
-	}
+	PORT = 9000
 }
